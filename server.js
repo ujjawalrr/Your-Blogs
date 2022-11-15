50f983c41acde76c95baf9f5cb8a44e655f43797
Ujjawal Kumar <ujjawalrr@gmail.com>
2022-11-15 16:40:18 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 const dbURI = "mongodb+srv://ujjawalrr1:test123@mongotuts.zclawp4.mongodb.net/mongo-tuts?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
- .then((result) => app.listen(3000))
+ .then((result) => app.listen(port))
  .catch((err) => console.log(err));
 
 app.set('view engine', 'ejs');
